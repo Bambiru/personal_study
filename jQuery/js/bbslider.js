38e1299ca90c641cc5cf1f9c14831ead0e0d04f7
bambiru <dj70083798@gmail.com>
2023-02-19 23:17:39 +0900

    jQ

---
+++ jQuery/js/bbslider.js
@@ -0,0 +1,16 @@
+$(function(){
+
+
+    $('.slide_gallery').bxSlider({
+        mode:'fade',
+        auto:true,
+        speed:1000,
+        pause:1500,
+        autoHover:true,
+        stopAutoOnClick:false,
+    });
+
+
+
+
+});
